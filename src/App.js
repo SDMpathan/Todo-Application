5c9b2423949509e836d5ea2eb85b00bda87db170
unknown <saddapathanjdk@gmail.com>
2021-08-04 21:05:30 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component{
+  state = {
+    inputValue:"",
+    todo:[],
+  }
+
+  inputChange = (e)=>{
+    this.setState({inputValue:e.target.value})
+  }
+
+  buttonSubmit = (e)=>{
+    e.preventDefault(); 
+    if(this.state.inputValue !== ""){
+    this.setState({
+      todo:[...this.state.todo,this.state.inputValue],
+      inputValue:"",
+    })
+  }
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        <div className="card">
+        <form onSubmit={this.buttonSubmit}>
+        <h1>ToDo App</h1>
+        <input type="text" 
+               placeholder="Enter task..."
+               value={this.state.inputValue}
+               onChange={this.inputChange} autoFocus/>
+        <button onClick={this.buttonSubmit}>Add Task</button>
+        <ol>
+          {this.state.todo.map((element, index) => (
+            <li key={index}>{element}</li>
+          ))}
+        </ol>
+        </form>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
-
-export default App;
